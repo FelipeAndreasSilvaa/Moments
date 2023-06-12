2c224d320a31aec68e4e90dfa4001b047fa362fa
Felipe Andreas Silva <you@example.com>
2023-06-11 21:28:37 -0300

    Criacao do front e validacao de formulario

---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.scss']
+})
+export class NewMomentComponent {
+  btnText = "Compartilhar!"
+
+}
